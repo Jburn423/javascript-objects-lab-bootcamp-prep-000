e61b22b48616c5259e769c75113b45c873d305e0
Jonah Burstein <jburstei@u.rochester.edu>
2018-01-24 23:35:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }destructivelyUpdateObjectWithKeyAndValue(recipes, 'prop2', 2);
 
 function deleteFromObjectByKey(object,key){
-  // var recipes = {'prop2': 2};
-  // var newRecipes = Object.assign({}, recipes);
-  // delete recipes[key];
-  // return recipes;
-  
   const newObject = Object.assign({}, object);
   delete newObject[key];
   return newObject
